af1585406a1df398e274ad6c3d479943276c6db7
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-02-13 08:41:12 +0200

    fhs

--- reactapp/src/dashboard/Analytics.jsx
+++ reactapp/src/dashboard/Analytics.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { MdContentCopy } from "react-icons/md";
-import { FaRegSquareCheck } from "react-icons/fa6";
+import { FaArrowDown, FaRegSquareCheck } from "react-icons/fa6";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { BsCalendarEvent } from "react-icons/bs";
+import { FaArrowUp } from "react-icons/fa6";
 import "./dashboard-styles/analytics.css"
+import { LuNotebook } from "react-icons/lu";
+import { TfiBag } from "react-icons/tfi";
+import { TbCircleDashedPercentage } from "react-icons/tb";
+import { useDarkMode } from "./context/DarkModeContext";
 const Analytics = () => {
+  const { darkMode } = useDarkMode();
   const cardData = [
     { title: "Orders", value: 201 ,icon:<MdContentCopy/>,perc:"20%"},
     { title: "Approved", value: 36 ,icon:<FaRegSquareCheck/>,perc:"8.5%"},
 
   ];
   const cardData1=[
-    { title: "Month Total", value: "25,410",icon:<BsCurrencyDollar/> },
-    { title: "Revenue", value: "1,352",icon:<BsCalendarEvent/> },
+    { title: "Month Total", value: "25,410",icon:<BsCurrencyDollar/>,perc:"0.2%" },
+    { title: "Revenue", value: "1,352",icon:<BsCalendarEvent/> ,perc:"2.4%"},
+  ]
+  const cardData2=[
+    { title: "Paid Invoice", value: "$25,410",icon:<LuNotebook/>,perc:"0.2%" },
+    { title: "Fund Received", value: "$145,652",icon:<TfiBag/> ,perc:"2.4%"},
   ]
 
   return (
@@ -25,6 +35,8 @@ const Analytics = () => {
         <div className={`stats-card card-${index + 1}`} key={index}>
           <div className="card-title"><span>{card.title}</span><span>{card.icon}</span></div>
           <p>{card.value}</p>
+          <div className="Arrow-Number"><span><FaArrowUp className="Up-icon" />{card.perc}</span> <div>since last month</div></div>
+         
         </div>
       ))}
       </div>
@@ -32,8 +44,10 @@ const Analytics = () => {
       {/* Cards section */}
       {cardData1.map((card, index) => (
         <div className={`stats-card card-${index + 1}`} key={index}>
-          <h3>{card.title}</h3>
+          <div className="card-title"><span>{card.title}</span><span>{card.icon}</span></div>
           <p>{card.value}</p>
+          <div className="Arrow-Number"><span className="Down-Perc"><FaArrowDown className="Down-icon" />{card.perc}</span> <div className="lastMonth">since last month</div></div>
+         
         </div>
       ))}
       </div>
@@ -45,10 +59,13 @@ const Analytics = () => {
         <div className="BarChartcolumn">Bar Chart</div>
         <div className="dashboard-container">
       {/* Cards section */}
-      {cardData1.map((card, index) => (
+      {cardData2.map((card, index) => (
         <div className={`stats-card card-${index + 1}`} key={index}>
-          <h3>{card.title}</h3>
-          <p>{card.value}</p>
+          <div className="card-title2"><span>{card.icon}</span><span className="circle-chart"><TbCircleDashedPercentage/></span></div>
+          <div className="title1">{card.title}</div>
+          <div className="value">{card.value}</div>
+          <div className="current-financial"> Current Financial Year</div>
+         
         </div>
       ))}
       </div>
