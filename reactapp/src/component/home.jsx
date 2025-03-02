990ab22173724f16bbd82bf25f1ea896295d0d99
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-03-02 21:48:03 +0200

    vendor done

--- reactapp/src/component/home.jsx
+++ reactapp/src/component/home.jsx
@@ -9,21 +9,21 @@ const Vendors=[
         id:1,
         name:"Santa Monica's Store",
         rating:5,
-        location:"Central Park, New York, United States (US)",
+        location:"New York,NY",
         image:"/SantaStore.jpg",
     },
     {
         id:2,
         name:" John Doe's Store",
         rating:4,
-        location:"Central Park, New York, United States (US)",
+        location:"New York,NY",
         image:"/JohnDoeStore.jpg",
     },
     {
         id:3,
         name:"Digital Good's Store",
         rating:5,
-        location:"Central Park, New York, United States (US)",
+        location:"New York, NY",
         image:"/DinapopStore.jpg",
     }
 ]
@@ -34,25 +34,38 @@ return (
             <Homediv2/>
             <NewArrival/>
             <div className="Main-Vendor1">
-                <h2>Our Vendor List</h2>
-                <div className="card-store1">
-                    {
-                        Vendors.map((vendor)=>
-                        {
-                            return(
-                            <div key={vendor.id} className="card1"> 
-                                <img src={vendor.image} alt={vendor.name} className="card-image12" />
-                                <div className="info-store1">
-                                  <div className="vendorname23">  <h3>{vendor.name}</h3> <div className="rightIcon2"><FaChevronRight/></div></div>
-                                    <div>{vendor.location}</div>
-                    
-                                </div>
+            <h4>Our Vendor List</h4>
+            <div className="card-store1">
+                {Vendors.map((vendor) => (
+                    <div key={vendor.id} className="card1">
+                        <img src={vendor.image} alt={vendor.name} className="card-image12" />
+                        <div className="vendorname23">
+                            <h3>{vendor.name}</h3>
+                            <div className="rightIcon2">
+                                <FaChevronRight className="iconog54" />
                             </div>
-                            )
-                        })
-                    }
-                </div>
+                        </div>
+                        <div className="vendor-location">{vendor.location}</div>
+                    </div>
+                ))}
             </div>
+            <div className="card-store12">
+                {Vendors.map((vendor) => (
+                    <div key={vendor.id} className="card1">
+                        <img src={vendor.image} alt={vendor.name} className="card-image12" />
+                        <div className="vendorname23">
+                            <h3>{vendor.name}</h3>
+                            <div className="rightIcon2">
+                                <FaChevronRight className="iconog54" />
+                            </div>
+                        </div>
+                        <div className="vendor-location">{vendor.location}</div>
+                    </div>
+                ))}
+                
+            </div>
+            <div className="vendorButton">SEE ALL VENDORS</div>
+        </div>
         </div>
     );
 }
