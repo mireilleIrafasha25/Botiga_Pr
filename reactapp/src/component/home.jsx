e50699398e398597ce4c1a1db4c4122246550659
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-03-02 22:28:45 +0200

    djf

--- reactapp/src/component/home.jsx
+++ reactapp/src/component/home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Homediv1 from "./Homediv1";
 import Homediv2 from "./Homediv2"
 import NewArrival from "./NewArrival";
+import { Link } from "react-router-dom";
 import { FaChevronRight } from "react-icons/fa";
+import ShopCard2 from "./shopcard2";
+import Shopcard1 from "./shopcard1";
+import ShopCard3 from "./shopcard3";
 import "../styles/vendorHome.css"
 const Vendors=[
     {
@@ -27,6 +31,32 @@ const Vendors=[
         image:"/DinapopStore.jpg",
     }
 ]
+const Images=[
+    {
+        id:1,
+        image:"/inst1.jpg",
+    },
+    {
+        id:2,
+        image:"/inst2.jpg",
+    },
+    {
+        id:3,
+        image:"/inst3.jpg",
+    },
+    {
+        id:4,
+        image:"/inst4.jpg",
+    },
+    {
+        id:5,
+        image:"/inst5.jpg",
+    },
+    {
+        id:6,
+        image:"/inst6.jpg",
+    }
+]
 const Home = () => {
 return (
         <div>
@@ -37,7 +67,7 @@ return (
             <h4>Our Vendor List</h4>
             <div className="card-store1">
                 {Vendors.map((vendor) => (
-                    <div key={vendor.id} className="card1">
+                    <div key={vendor.id} className="card14">
                         <img src={vendor.image} alt={vendor.name} className="card-image12" />
                         <div className="vendorname23">
                             <h3>{vendor.name}</h3>
@@ -51,7 +81,7 @@ return (
             </div>
             <div className="card-store12">
                 {Vendors.map((vendor) => (
-                    <div key={vendor.id} className="card1">
+                    <div key={vendor.id} className="card14">
                         <img src={vendor.image} alt={vendor.name} className="card-image12" />
                         <div className="vendorname23">
                             <h3>{vendor.name}</h3>
@@ -64,7 +94,25 @@ return (
                 ))}
                 
             </div>
-            <div className="vendorButton">SEE ALL VENDORS</div>
+            <Link to="/vendor" style={{textDecoration:"none"}}><div className="vendorButton">SEE ALL VENDORS</div></Link>
+        </div>
+        <div className="Main-ExploreProduct">
+            <div className="explore-product9">Explore Our Products</div>
+            <Shopcard1/>
+            <ShopCard2/>
+            <ShopCard3/>
+            <Link to="/shop" style={{textDecoration:"none"}}><div className="productButton12">VIEW ALL PRODUCTS</div></Link>
+        </div>
+        <div>
+            <div>Follow us on @instagram</div>
+            <div className="Card-ImageInst">
+                {Images.map((vendor)=>
+                {
+                    return(
+                        <img key={vendor.id} src={vendor.image} alt={vendor.name} className="card-image15"/>
+                    )
+                })}
+            </div>
         </div>
         </div>
     );
